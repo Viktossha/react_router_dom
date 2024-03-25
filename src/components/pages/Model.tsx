7821947eac2f2646fb77ed09ce23c06a8e3eae21
Vika <vikapetrova8@gmail.com>
2024-03-25 20:19:34 +0300

    fix

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -7,51 +7,55 @@ type ModelPropsType = {
 }
 const Model = (props: ModelPropsType) => {
     const params = useParams();
+    const currentModel = props.array.find(el => el.id === params.id);
 
-    // adidasArr.find(el => {
-    //     if (el.id === params.id) {
-    //         return <div style={{textAlign: "center"}}>
-    //             <h3>{el.model}</h3>
-    //             <div>{el.collection}</div>
-    //             <img src={el.picture} alt={el.model}/>
-    //             <div>{el.price}</div>
-    //         </div>
-    //     } else {
-    //         return <div>Модель отсутствует </div>
-    //     }})
-
-
-
-    if (props.array.find(el => el.id === params.id)) {
-        return (
-            <div style={{textAlign: "center"}}>
-                <h3>{props.array[Number(params.id)].model}</h3>
-                        <div>{props.array[Number(params.id)].collection}</div>
-                        <img src={props.array[Number(params.id)].picture} alt={props.array[Number(params.id)].model}/>
-                       <div>{props.array[Number(params.id)].price}</div>
-            </div>
-        )
-    } else {
-        return <div>Модель отсутствует </div>
-    }
+    return (
+        <div style={{textAlign: "center"}}>
+            {currentModel
+                ?
+                <>
+                    <h3>{currentModel.model}</h3>
+                    <div>{currentModel.collection}</div>
+                    <img src={currentModel.picture} alt={currentModel.model}/>
+                    <div>{currentModel.price}</div>
+                </>
+                :
+                <div>Модель отсутствует </div>
+            }
+        </div>
+    )
 
 
-    // return (
-    //     <div style={{textAlign: "center"}}>
-    //         {/*{adidasArr.map(el => {*/}
-    //         {/*    if (el.id === params.id) {*/}
-    //         {/*        return <div>*/}
-    //         {/*            <h3>{el.model}</h3>*/}
-    //         {/*            <div>{el.collection}</div>*/}
-    //         {/*            <img src={el.picture} alt={el.model}/>*/}
-    //         {/*            <div>{el.price}</div>*/}
-    //         {/*        </div>*/}
-    //         {/*    } else {*/}
-    //         {/*        return <div>Модель отсутствует </div>*/}
-    //         {/*    }*/}
-    //         {/*})}*/}
-    //     </div>
-    // );
-};
+// adidasArr.find(el => {
+//     if (el.id === params.id) {
+//         return <div style={{textAlign: "center"}}>
+//             <h3>{el.model}</h3>
+//             <div>{el.collection}</div>
+//             <img src={el.picture} alt={el.model}/>
+//             <div>{el.price}</div>
+//         </div>
+//     } else {
+//         return <div>Модель отсутствует </div>
+//     }})
+
+
+// return (
+//     <div style={{textAlign: "center"}}>
+//         {/*{adidasArr.map(el => {*/}
+//         {/*    if (el.id === params.id) {*/}
+//         {/*        return <div>*/}
+//         {/*            <h3>{el.model}</h3>*/}
+//         {/*            <div>{el.collection}</div>*/}
+//         {/*            <img src={el.picture} alt={el.model}/>*/}
+//         {/*            <div>{el.price}</div>*/}
+//         {/*        </div>*/}
+//         {/*    } else {*/}
+//         {/*        return <div>Модель отсутствует </div>*/}
+//         {/*    }*/}
+//         {/*})}*/}
+//     </div>
+// );
+}
+;
 
 export default Model;
