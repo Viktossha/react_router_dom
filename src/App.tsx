dacf378fe8ed2bfcee06411dc2a36d27f85d1c2b
Vika <vikapetrova8@gmail.com>
2024-10-20 15:56:25 +0300

    Error404, Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
@@ -16,9 +17,14 @@ function App() {
                 </div>
                 <div className={styles.content}>
                     <Routes>
+                        <Route path="/" element={<Navigate to={"/page1"}/>}/>
+
                         <Route path="/page1" element={<PageOne/>}/>
                         <Route path="/page2" element={<PageTwo/>}/>
                         <Route path="/page3" element={<PageThree/>}/>
+
+                        <Route path="/404" element={<Error404/>}/>
+                        <Route path="/*" element={<Navigate to={"/404"}/>}/>
                     </Routes>
 
                 </div>
