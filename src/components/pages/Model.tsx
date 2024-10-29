0f93c37ea1559f5001e21a3489400de9cb54d749
Vika <vikapetrova8@gmail.com>
2024-10-29 23:59:37 +0300

    fix Adidas

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -5,14 +5,18 @@ import {useParams} from "react-router-dom";
 export const Model = () => {
     const params = useParams()
     let id = Number(params.id)
+    let currentModel = adidasArr.find(el => el.id === id)
     console.log(params)
     return (
-        <div style={{display: 'flex', flexDirection:'column', alignItems: 'center'}}>
-            <h2>{adidasArr[id].model}</h2>
-            <h3>{adidasArr[id].collection}</h3>
-            <img src={adidasArr[id].picture} alt={adidasArr[id].model}/>
-            <p>{adidasArr[id].price}</p>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            {currentModel
+                ? <>
+                    <h2>{currentModel.model}</h2>
+                    <h3>{currentModel.collection}</h3>
+                    <img src={currentModel.picture} alt={currentModel.model}/>
+                    <p>{currentModel.price}</p>
+                </>
+                : <h3>Модель отсутствует</h3>}
         </div>
-
     );
 };
